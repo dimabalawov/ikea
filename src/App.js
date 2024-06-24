b76dc9c1a38d0009977199ea045f2d997a3b3ac4
dimabalawov <dimabalawov@gmail.com>
2024-06-25 00:59:07 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./Pages/Components/Header/header";
 import Footer from "./Pages/Components/Footer/Footer";
 import MainPage from "./Pages/Main_page/MainPage";
 import RegistrationPage from "./Pages/RegistrationPage/RegistrationPage";
-import LoginPage from './Pages/LoginPage/LoginPage'
+import LoginPage from './Pages/LoginPage/LoginPage';
 import ProductPage from "./Pages/ProductPage/ProductPage";
 import DesignPage from "./Pages/DesignPage/DesignPage";
-import RoomsPage from "./Pages/RoomsPage/RoomsPage"
-
-import './App.css'
+import RoomsPage from "./Pages/RoomsPage/RoomsPage";
+import './App.css';
 import IdeaPage from "./Pages/Idea_Page/IdeaPage";
 import EmptyPage from "./Pages/EmptyPage/EmptyPage";
 
@@ -18,63 +16,53 @@ function App() {
   const [currentPage, setCurrentPage] = useState("/");
 
   return (
-    <BrowserRouter>
-    <div className="App">
-    {(currentPage !== "registration" && currentPage !== 'login' && currentPage !=='/' && currentPage != '/design')  && <Header />}
-      <div>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={<MainPage setCurrentPage={setCurrentPage} />}
-          />
-          <Route
-            exact
-            path="/registration"
-            element={<RegistrationPage setCurrentPage={setCurrentPage} />}
-          />
-
-          <Route
-            exact
-            path="/login"
-            element={<LoginPage setCurrentPage={setCurrentPage} />}
-          />
-
-          <Route
-          exact path = "/ideas"
-          element = {<IdeaPage setCurrentPage = {setCurrentPage}/>}
-          />  
-
-          <Route
-          exact path = "/products"
-          element = {<ProductPage setCurrentPage = {setCurrentPage}/>}
-          />
-          <Route
-          exact path = "/rooms"
-          element = {<RoomsPage setCurrentPage = {setCurrentPage}/>}
-          />
-
-          <Route
-          exact path = "/design"
-          element = {<DesignPage setCurrentPage = {setCurrentPage}/>}
-          />
-
-          <Route
-          exact path = "/stash"
-          element = {<EmptyPage title = "Ваша корзина пуста..." setCurrentPage = {setCurrentPage}></EmptyPage>}
-          />
-
-          <Route
-          exact path = "/wishlist"
-          element = {<EmptyPage title = "Ваш лист бажань пустий..." setCurrentPage = {setCurrentPage}></EmptyPage>}
-          />
-
-        </Routes>
+    <Router>
+      <div className="App">
+        {(currentPage !== "registration" && currentPage !== 'login' && currentPage !=='/' && currentPage !== '/design') && <Header />}
+        <div>
+          <Routes>
+            <Route
+              path="/"
+              element={<MainPage setCurrentPage={setCurrentPage} />}
+            />
+            <Route
+              path="/registration"
+              element={<RegistrationPage setCurrentPage={setCurrentPage} />}
+            />
+            <Route
+              path="/login"
+              element={<LoginPage setCurrentPage={setCurrentPage} />}
+            />
+            <Route
+              path="/ideas"
+              element={<IdeaPage setCurrentPage={setCurrentPage} />}
+            />
+            <Route
+              path="/products"
+              element={<ProductPage setCurrentPage={setCurrentPage} />}
+            />
+            <Route
+              path="/rooms"
+              element={<RoomsPage setCurrentPage={setCurrentPage} />}
+            />
+            <Route
+              path="/design"
+              element={<DesignPage setCurrentPage={setCurrentPage} />}
+            />
+            <Route
+              path="/stash"
+              element={<EmptyPage title="Ваша корзина пуста..." setCurrentPage={setCurrentPage} />}
+            />
+            <Route
+              path="/wishlist"
+              element={<EmptyPage title="Ваш лист бажань пустий..." setCurrentPage={setCurrentPage} />}
+            />
+          </Routes>
+        </div>
+        {(currentPage !== "registration" && currentPage !== 'login') && <Footer />}
       </div>
-      {(currentPage !== "registration" && currentPage !== 'login')  && <Footer />}
-    </div>
-    </BrowserRouter>
+    </Router>
   );
-} 
+}
 
 export default App;
